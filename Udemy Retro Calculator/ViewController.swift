5f498022b5eda87a5bd96fcf901f78ae49cc8841
Manuela Danz <niklasdanz@p20030051af58711caebc32fffecb61b7.dip0.t-ipconnect.de>
2016-09-16 22:32:07 +0200

    Initial Commit

---
+++ Udemy Retro Calculator/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Udemy Retro Calculator
+//
+//  Created by Niklas Danz on 16.09.16.
+//  Copyright © 2016 Niklas Danz. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
